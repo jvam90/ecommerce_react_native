7f17ad196b0ab5dcfd664a85d473c55c788611bf
jvam90 <jvam90@gmail.com>
2023-01-12 19:07:47 -0300

    Notas e tela principal do app.

--- src/Telas/TelaPrincipal/Index.js
+++ src/Telas/TelaPrincipal/Index.js
@@ -1,5 +1,10 @@
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import logo from "../../../assets/tela_principal.png";
+
+function teste() {
+  alert("Oi");
+}
+
 function TelaPrincipal() {
   return (
     <View>
@@ -9,6 +14,14 @@ function TelaPrincipal() {
         Venda seus produtos de maneira mais inteligente e rápida para obter
         dinheiro imediato e uma consciência mais limpa.
       </Text>
+      <View style={estilos.botoes}>
+        <TouchableOpacity style={estilos.entrar} onPress={teste}>
+          <Text style={estilos.textoEntrar}>Entrar</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={estilos.cadastrar}>
+          <Text style={estilos.textoCadastrar}>Cadastrar</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -24,6 +37,39 @@ const estilos = StyleSheet.create({
     margin: 25,
     marginTop: 0,
     fontSize: 16,
+  },
+  botoes: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 25,
+  },
+  entrar: {
+    backgroundColor: "#ff7a00",
+    padding: 10,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "#ff7a00",
+    width: 160,
+    marginStart: 25,
+  },
+  cadastrar: {
+    backgroundColor: "#ffffff",
+    padding: 10,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "#ff7a00",
+    width: 160,
+    marginEnd: 25,
+  },
+  textoEntrar: {
+    textAlign: "center",
+    color: "#fff",
+    fontWeight: "bold",
+  },
+  textoCadastrar: {
+    textAlign: "center",
+    color: "#ff7a00",
+    fontWeight: "bold",
   },
 });
 
