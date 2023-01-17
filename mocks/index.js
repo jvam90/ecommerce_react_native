9ed37f471d39a20739f8cf5490432190a56da3a4
jvam90 <jvam90@gmail.com>
2023-01-17 15:06:20 -0300

    Adicionar mocks de dados.

---
+++ mocks/index.js
@@ -0,0 +1,20 @@
+export const anuncios = [
+  {
+    id: 1,
+    titulo: "Titulo 1",
+    descricao: "Descrição 1",
+    textoBotao: "Saiba Mais 1",
+  },
+  {
+    id: 2,
+    titulo: "Titulo 2",
+    descricao: "Descrição 2",
+    textoBotao: "Saiba Mais 2",
+  },
+  {
+    id: 3,
+    titulo: "Titulo 3",
+    descricao: "Descrição 3",
+    textoBotao: "Saiba Mais 3",
+  },
+];
