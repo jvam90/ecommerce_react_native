faf57c83714712cc162ced1aa3bea6ae5407e827
jvam90 <jvam90@gmail.com>
2023-01-19 21:30:07 -0300

    Adicionar secao de review em produto.

--- src/Telas/Produto/Index.js
+++ src/Telas/Produto/Index.js
@@ -1,16 +1,11 @@
-import {
-  Image,
-  SafeAreaView,
-  StyleSheet,
-  TouchableOpacity,
-  View,
-} from "react-native";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import modelo1 from "../../../assets/modelo_1.png";
 import modelo2 from "../../../assets/modelo_2.png";
 import modelo3 from "../../../assets/modelo_3.png";
 import modelo4 from "../../../assets/modelo_4.png";
 import Carrousel from "./componentes/Carrousel";
 import Miniatura from "./componentes/Miniatura";
+import AntIcon from "react-native-vector-icons/AntDesign";
 
 const imagens = [modelo1, modelo2, modelo3, modelo4];
 
@@ -31,6 +26,16 @@ function Produto() {
               );
             })}
           </View>
+          <View style={estilos.review}>
+            <AntIcon name="star" size={20} color={"#fdcc0d"}></AntIcon>
+            <Text style={estilos.nota}>4.9</Text>
+            <Text style={estilos.quantidade}>
+              40 <Text style={estilos.textoReview}>Reviews</Text>
+            </Text>
+          </View>
+          <Text style={estilos.produto}>Camisa de Algodão</Text>
+          <Text style={estilos.preco}>R$ 100,00</Text>
+          <Text style={estilos.tamanho}>Selecionar Tamanho</Text>
         </View>
       </SafeAreaView>
     </>
@@ -45,6 +50,35 @@ const estilos = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
     marginBottom: 15,
+  },
+  review: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  nota: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginStart: 5,
+  },
+  quantidade: {
+    marginStart: 5,
+    color: "#cacaca",
+  },
+  textoReview: {
+    color: "#000",
+  },
+  produto: {
+    fontSize: 16,
+    marginTop: 10,
+  },
+  preco: {
+    fontSize: 18,
+    marginTop: 10,
+    fontWeight: "bold",
+  },
+  tamanho: {
+    fontSize: 16,
+    marginTop: 15,
   },
 });
 
