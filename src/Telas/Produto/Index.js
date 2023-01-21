e84ab27e8745a8abc6eef2cb07d14af17cd62645
jvam90 <jvam90@gmail.com>
2023-01-21 14:02:34 -0300

    Terminar tela de produto.

--- src/Telas/Produto/Index.js
+++ src/Telas/Produto/Index.js
@@ -1,4 +1,10 @@
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+} from "react-native";
 import modelo1 from "../../../assets/modelo_1.png";
 import modelo2 from "../../../assets/modelo_2.png";
 import modelo3 from "../../../assets/modelo_3.png";
@@ -6,10 +12,12 @@ import modelo4 from "../../../assets/modelo_4.png";
 import Carrousel from "./componentes/Carrousel";
 import Miniatura from "./componentes/Miniatura";
 import AntIcon from "react-native-vector-icons/AntDesign";
+import { useState } from "react";
 
 const imagens = [modelo1, modelo2, modelo3, modelo4];
 
 function Produto() {
+  const [tamanho, setTamanho] = useState("");
   return (
     <>
       <SafeAreaView>
@@ -36,6 +44,87 @@ function Produto() {
           <Text style={estilos.produto}>Camisa de Algodão</Text>
           <Text style={estilos.preco}>R$ 100,00</Text>
           <Text style={estilos.tamanho}>Selecionar Tamanho</Text>
+
+          <View style={estilos.botoesTamanho}>
+            <TouchableOpacity
+              style={[
+                estilos.botaoTamanho,
+                { marginEnd: 10 },
+                tamanho === "P" && estilos.botaoSelecionado,
+              ]}
+              onPress={() => setTamanho("P")}
+            >
+              <Text style={{ fontSize: 16 }}>P</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={[
+                estilos.botaoTamanho,
+                { marginHorizontal: 10 },
+                tamanho === "M" && estilos.botaoSelecionado,
+              ]}
+              onPress={() => setTamanho("M")}
+            >
+              <Text style={{ fontSize: 16 }}>M</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              style={[
+                estilos.botaoTamanho,
+                { marginHorizontal: 10 },
+                tamanho === "G" && estilos.botaoSelecionado,
+              ]}
+              onPress={() => setTamanho("G")}
+            >
+              <Text style={{ fontSize: 16 }}>G</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={[
+                estilos.botaoTamanho,
+                { marginHorizontal: 10 },
+                tamanho === "GG" && estilos.botaoSelecionado,
+              ]}
+              onPress={() => setTamanho("GG")}
+            >
+              <Text style={{ fontSize: 16 }}>GG</Text>
+            </TouchableOpacity>
+          </View>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "space-between",
+              marginTop: 25,
+            }}
+          >
+            <View style={estilos.botao}>
+              <View style={{ flexDirection: "row", alignItems: "center" }}>
+                <TouchableOpacity
+                  style={[estilos.botaoNumItens, { marginHorizontal: 10 }]}
+                  onPress={() => {}}
+                >
+                  <AntIcon name="minus" size={15} color="#000000" />
+                </TouchableOpacity>
+
+                <Text style={{ fontSize: 20, fontWeight: "bold" }}>1</Text>
+
+                <TouchableOpacity
+                  style={[estilos.botaoNumItens, { marginHorizontal: 10 }]}
+                  onPress={() => {}}
+                >
+                  <AntIcon name="plus" size={15} color="#000000" />
+                </TouchableOpacity>
+              </View>
+            </View>
+
+            <TouchableOpacity
+              style={estilos.botaoAddCarrinho}
+              onPress={() => {}}
+            >
+              <Text style={estilos.textoBotaoAddCarrinho}>
+                Adicionar ao Carrinho
+              </Text>
+            </TouchableOpacity>
+          </View>
         </View>
       </SafeAreaView>
     </>
@@ -79,6 +168,43 @@ const estilos = StyleSheet.create({
   tamanho: {
     fontSize: 16,
     marginTop: 15,
+  },
+  botoesTamanho: {
+    flexDirection: "row",
+    marginTop: 15,
+  },
+  botaoTamanho: {
+    borderWidth: 1,
+    borderColor: "#CACACA",
+    borderRadius: 5,
+    padding: 20,
+  },
+  botaoSelecionado: {
+    backgroundColor: "#FFA959",
+    borderColor: "#FFA959",
+  },
+  botaoNumItens: {
+    borderWidth: 1,
+    borderColor: "#CACACA",
+    borderRadius: 5,
+    padding: 10,
+    backgroundColor: "#FFFFFF",
+  },
+  botaoAddCarrinho: {
+    backgroundColor: "#FF7A00",
+    padding: 10,
+    alignSelf: "stretch",
+    justifyContent: "center",
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: "#FF7A00",
+    width: 180,
+    marginStart: 20,
+  },
+  textoBotaoAddCarrinho: {
+    textAlign: "center",
+    color: "#FFFFFF",
+    fontWeight: "bold",
   },
 });
 
